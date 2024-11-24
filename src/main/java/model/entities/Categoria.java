c19409e28b5ad702452c18c290250677b4504824
Bruno <111437933+brunotemote@users.noreply.github.com>
2024-11-24 16:00:55 -0300

    Update Categoria.java

--- src/main/java/model/entities/Categoria.java
+++ src/main/java/model/entities/Categoria.java
@@ -1,25 +1,26 @@
-package model.entities;
+package model.entities; // pacote da entidade
 
-import java.io.Serializable;
-import java.util.Objects;
+import java.io.Serializable; // importa a interface Serializable
+import java.util.Objects; // importa a classe Objects
 
-public class Categoria implements Serializable {
+public class Categoria implements Serializable { // classe que representa uma categoria, implementa Serializable para permitir a serialização do objeto
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L; // número de versão da classe para serialização
 
-    private Integer id;
-    private String nome;
-    private String descricao;
+    private Integer id; // identificador único da categoria
+    private String nome; // nome da categoria
+    private String descricao; // descrição da categoria
 
-    public Categoria() {
+    public Categoria() { // construtor padrão
     }
 
-    public Categoria(String descricao, Integer id, String nome) {
+    public Categoria(String descricao, Integer id, String nome) { // construtor com parâmetros
         this.descricao = descricao;
         this.id = id;
         this.nome = nome;
     }
 
+    // getters e setters para os atributos da classe
     public String getDescricao() {
         return descricao;
     }
@@ -45,19 +46,19 @@ public class Categoria implements Serializable {
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o) { // sobrescreve o método equals para comparar categorias pelo nome
         if (o == null || getClass() != o.getClass()) return false;
         Categoria categoria = (Categoria) o;
         return Objects.equals(nome, categoria.nome);
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode() { // sobrescreve o método hashCode para gerar um código hash baseado no nome
         return Objects.hashCode(nome);
     }
 
     @Override
-    public String toString() {
+    public String toString() { // sobrescreve o método toString para retornar uma representação em string da categoria
         return "Categoria{" +
                 "descricao='" + descricao + '\'' +
                 ", id=" + id +

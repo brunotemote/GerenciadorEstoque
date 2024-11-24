e0139d1606c34df45c0d22141624fe6e7f41e598
Bruno <111437933+brunotemote@users.noreply.github.com>
2024-11-24 15:55:09 -0300

    Update Program.java

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,12 +1,11 @@
-package application;
+package application; // pacote da aplicação
 
+import view.Menu; // importa a classe Menu do pacote view
 
-import view.Menu;
+public class Program { // classe principal do programa
 
-public class Program {
-
-    public static void main(String[] args) {
-        Menu menu = new Menu();
-        menu.setVisible(true);
+    public static void main(String[] args) { // método principal que inicia a aplicação
+        Menu menu = new Menu(); // cria um novo objeto Menu
+        menu.setVisible(true); // torna o menu visível para o usuário
     }
 }

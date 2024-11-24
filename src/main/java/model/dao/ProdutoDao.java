a3931fedeb9ab089a644e42e9674cbbcc0d396c2
Bruno <111437933+brunotemote@users.noreply.github.com>
2024-11-24 15:57:21 -0300

    Update ProdutoDao.java

--- src/main/java/model/dao/ProdutoDao.java
+++ src/main/java/model/dao/ProdutoDao.java
@@ -1,18 +1,17 @@
-package model.dao;
+package model.dao; // pacote do DAO (Data Access Object)
 
-import model.entities.Categoria;
-import model.entities.Produto;
+import model.entities.Categoria; // importa a classe Categoria
+import model.entities.Produto; // importa a classe Produto
 
-import java.util.List;
+import java.util.List; // importa a interface List
 
-public interface ProdutoDao {
+public interface ProdutoDao { // interface que define as operações de acesso a dados para a entidade Produto
 
-    boolean insert(Produto obj);
-    Integer update(String nome, String valorCompra, String valorVenda, String quantidade);
-    Integer deleteByName(String name);
-    Produto findByName(String name);
-    Produto findByNameAndCategory(String name, Categoria categoria);
-    List<Produto> findAll();
-    List<Produto> findAllByCategoria(Categoria categoria);
-
+    boolean insert(Produto obj); // insere um novo produto no banco de dados
+    Integer update(String nome, String valorCompra, String valorVenda, String quantidade); // atualiza um produto no banco de dados
+    Integer deleteByName(String name); // exclui um produto do banco de dados pelo nome
+    Produto findByName(String name); // busca um produto no banco de dados pelo nome
+    Produto findByNameAndCategory(String name, Categoria categoria); // busca um produto pelo nome e categoria
+    List<Produto> findAll(); // busca todos os produtos do banco de dados
+    List<Produto> findAllByCategoria(Categoria categoria); // busca todos os produtos de uma categoria
 }

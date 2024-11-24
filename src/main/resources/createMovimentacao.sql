c06daa261aa05cca5574e3543f2e3de983dc54d4
Bruno <111437933+brunotemote@users.noreply.github.com>
2024-11-24 15:31:35 -0300

    Update createMovimentacao.sql

--- src/main/resources/createMovimentacao.sql
+++ src/main/resources/createMovimentacao.sql
@@ -1,8 +1,10 @@
+
+-- cria a tabela movimentacao caso ela nao exista
 CREATE TABLE IF NOT EXISTS Movimentacao (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    produto_id INT,
-    tipo_movimentacao ENUM('entrada', 'saida'),
-    quantidade INT,
-    data_movimentacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (produto_id) REFERENCES Produto(id)
+    id INT AUTO_INCREMENT PRIMARY KEY,  -- id da movimentacao, inteiro, autoincrementado e chave primaria
+    produto_id INT,  -- id do produto relacionado a movimentacao, inteiro
+    tipo_movimentacao ENUM('entrada', 'saida'),  -- tipo da movimentacao: entrada ou saida
+    quantidade INT,  -- quantidade de produtos movimentados, inteiro
+    data_movimentacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  -- data e hora da movimentacao, com valor padrao definido como a data e hora atuais
+    FOREIGN KEY (produto_id) REFERENCES Produto(id)  -- chave estrangeira que referencia a tabela produto pelo id
 );

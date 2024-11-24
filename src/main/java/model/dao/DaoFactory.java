6c65960bbfdb7291a67f73c6de52134ed613349c
Bruno <111437933+brunotemote@users.noreply.github.com>
2024-11-24 15:56:43 -0300

    Update DaoFactory.java

--- src/main/java/model/dao/DaoFactory.java
+++ src/main/java/model/dao/DaoFactory.java
@@ -1,17 +1,16 @@
-package model.dao;
+package model.dao; // pacote do DAO (Data Access Object)
 
-import model.impl.CategoriaDaoJDBC;
-import model.impl.ProdutoDaoJDBC;
-import util.DB;
+import model.impl.CategoriaDaoJDBC; // importa a classe CategoriaDaoJDBC
+import model.impl.ProdutoDaoJDBC; // importa a classe ProdutoDaoJDBC
+import util.DB; // importa a classe DB
 
-public class DaoFactory {
+public class DaoFactory { // classe que cria instâncias de DAOs
 
-    public static ProdutoDao createProdutoDao(){
-        return new ProdutoDaoJDBC(DB.getConnection());
+    public static ProdutoDao createProdutoDao(){ // cria uma instância de ProdutoDao
+        return new ProdutoDaoJDBC(DB.getConnection()); // retorna uma nova instância de ProdutoDaoJDBC com a conexão do banco de dados
     }
 
-    public static CategoriaDao createCategoriaDao(){
-        return new CategoriaDaoJDBC(DB.getConnection());
+    public static CategoriaDao createCategoriaDao(){ // cria uma instância de CategoriaDao
+        return new CategoriaDaoJDBC(DB.getConnection()); // retorna uma nova instância de CategoriaDaoJDBC com a conexão do banco de dados
     }
-
 }

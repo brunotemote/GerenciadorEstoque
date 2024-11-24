c80cc9dd116e88c64c469856e65bfe9c0cad1119
Bruno <111437933+brunotemote@users.noreply.github.com>
2024-11-24 15:56:05 -0300

    Update CategoriaDao.java

--- src/main/java/model/dao/CategoriaDao.java
+++ src/main/java/model/dao/CategoriaDao.java
@@ -1,15 +1,14 @@
-package model.dao;
+package model.dao; // pacote do DAO (Data Access Object)
 
-import model.entities.Categoria;
+import model.entities.Categoria; // importa a classe Categoria
 
-import java.util.List;
+import java.util.List; // importa a interface List
 
-public interface CategoriaDao {
+public interface CategoriaDao { // interface que define as operações de acesso a dados para a entidade Categoria
 
-    void insert(Categoria obj);
-    Integer update(String novoNome, String novaDescricao, String antigoNome);
-    void deleteByName(String name);
-    Categoria findByName(String name);
-    List<Categoria> findAll();
-
+    void insert(Categoria obj); // insere uma nova categoria no banco de dados
+    Integer update(String novoNome, String novaDescricao, String antigoNome); // atualiza uma categoria no banco de dados
+    void deleteByName(String name); // exclui uma categoria do banco de dados pelo nome
+    Categoria findByName(String name); // busca uma categoria no banco de dados pelo nome
+    List<Categoria> findAll(); // busca todas as categorias do banco de dados
 }
